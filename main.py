bbc79e29aba70e6ea949a388670222bad5cb3b93
root <root@DESKTOP-QU1IG9C.localdomain>
2020-12-01 12:02:31 -0700

    I just had to start with b16 instead of b64, duh

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import base64
 
+file = open("encodedflag.txt", "r")
+
+read = file.readline()
+
+encrypted = ""
+
+for i in range(0,5):
+    encrypted = base64.b16decode(read)
+    read = encrypted
+
+for i in range(0,5):
+    encrypted = base64.b32decode(read)
+    read = encrypted
+
+for i in range(0,5):
+    encrypted = base64.b64decode(read)
+    read = encrypted
+
+print(read)
+
+file.close()
